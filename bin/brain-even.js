927b42e7349ac13bd23ddd6ba522b682c1489fb7
ivanstarodubtcev <loteran@icloud.com>
2022-06-23 08:08:39 +0300

    fix README.md (asciinema), add bin/brain-even.js,  fix package.json, fix src/cli.js, add src/even-game.js

---
+++ bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import greeting from '../src/cli.js';
+import evenGame from '../src/even-game.js';
+
+console.log('Welcome to the Brain Games!');
+const name = greeting();
+evenGame(name);
