550d27531a0d5479a32e91dc12d592fd4f4a727b
ivanstarodubtcev <loteran@icloud.com>
2022-06-29 07:59:56 +0300

    add bin/brain-prime.js, add src/games/prime-games.js, fix package.json

---
+++ src/games/prime-games.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+import getRandomNumber from '../getRandomNumber.js';
+import gameLogic from '../index.js';
+import greeting from '../cli.js';
+
+const primeGame = () => {
+  const name = greeting();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  const oneRound = () => {
+    const number1 = getRandomNumber();
+    let correctAnswer = '';
+    for (let i = 2; i <= number1 / 2; i += 1) {
+      if (number1 % i !== 0) {
+        correctAnswer = 'yes';
+      } else {
+        correctAnswer = 'no';
+        i = number1;
+      }
+    }
+
+    const question = readlineSync.question(`Question: ${number1} \nYour answer: `);
+    return [question, correctAnswer];
+  };
+  gameLogic(oneRound, name);
+};
+
+export default primeGame;
