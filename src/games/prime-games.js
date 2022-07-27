983d31b11e706675e670af752d1e03c2a8ace3f2
ivanstarodubtcev <loteran@icloud.com>
2022-07-27 12:13:40 +0300

    fix src/games/prime-games.js add numbers < 2

--- src/games/prime-games.js
+++ src/games/prime-games.js
@@ -4,6 +4,9 @@ import gameRun from '../index.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
   for (let minDivider = 2; minDivider < num; minDivider += 1) {
     if (num % minDivider === 0) {
       return false;
